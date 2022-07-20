e1706ee1c0881d2dce01716515cff08e8c01502a
Christopher Dearie <cdearie@bloomberg.net>
2022-07-20 15:28:13 -0400

    Add Dev group

--- iam_groups.tf
+++ iam_groups.tf
@@ -2,3 +2,8 @@ resource "aws_iam_group" "empire_developers" {
   name = "Empire-Developers"
   path = "/users/"
 }
+
+resource "aws_iam_group" "empire_cloud_engineers" {
+  name = "Empire-Cloud-Engineers"
+  path = "/users/"
+}
